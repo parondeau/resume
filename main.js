636ec9239bb8239a29715d728e1d89a045a5162c
Paul Rondeau <paulrondeau07@gmail.com>
2014-10-29 18:43:52 +0100

    almost done personal website, TODO: add resume link to bottom

--- main.js
+++ main.js
@@ -1,3 +1,52 @@
 $(document).ready(function() {
+	$('#gradientTop').css('height', ($('header').height()));
 	$('main').css('margin-top', ($('header').height()));
+
+	// javascript
+	var dataJS = [
+		{
+			value: 85,
+			color: "#b72200"
+		}, 
+		{
+			value: 15,
+			color: "#eee"
+		}
+	]
+	// python
+	var dataPY = [
+		{
+			value: 75,
+			color: "#b72200"
+		}, 
+		{
+			value: 25,
+			color: "#eee"
+		}
+	]
+	// c
+	var dataC = [
+		{
+			value: 40,
+			color: "#b72200"
+		}, 
+		{
+			value: 60,
+			color: "#eee"
+		}
+	]
+	var options = {
+		percentageInnerCutout: 90,
+	    animation: false,
+	    responsive: true
+	}
+	var ctx = $("#skillChartJS").get(0).getContext("2d");
+	var myDoughnutChart = new Chart(ctx).Doughnut(dataJS, options);
+
+	var ctx = $("#skillChartPY").get(0).getContext("2d");
+	var myDoughnutChart = new Chart(ctx).Doughnut(dataPY, options);
+
+	var ctx = $("#skillChartC").get(0).getContext("2d");
+	var myDoughnutChart = new Chart(ctx).Doughnut(dataC, options);
+
 });
